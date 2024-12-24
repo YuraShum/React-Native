3e7acd663824d54a14d6c9c8547073d66985617a
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-24 14:09:57 +0200

    feat: user registration page created

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,13 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, ScrollView, Image, Text } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import FormField from '@/components/FormField'
+import CustomButton from '@/components/(tabs)/CustomButton'
+import { Link } from 'expo-router'
 
 type Props = {}
 
 const SignUp = (props: Props) => {
+
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleChangeText = (event: string) => {
+    setForm({
+      ...form,
+      email: event
+    })
+  }
+
+  const handleChangePassword = (event: string) => {
+    setForm({
+      ...form,
+      password: event
+    })
+  }
+  const handleChangeUsername = (event: string) => {
+    setForm({
+      ...form,
+      username: event
+    })
+  }
+
+  const submit = () => { }
   return (
-    <View>
-      <Text>sign-up</Text>
-    </View>
+    <SafeAreaView
+      className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]' />
+          <Text
+            className='text-2xl text-white mt-10 font-psemibold'>
+            Sign up to Aora
+          </Text>
+
+          {/* username section */}
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeField={(event) => handleChangeUsername(event)}
+            otherStyles="mt-7"
+            placeholder='Enter your username' />
+          {/* email section */}
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeField={(event) => handleChangeText(event)}
+            otherStyles="mt-7"
+            keyboardType='email-address'
+            placeholder='Enter your email' />
+
+          {/* password section */}
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeField={(event) => handleChangePassword(event)}
+            otherStyles="mt-7"
+            placeholder='Enter your password' />
+
+          <CustomButton
+            title='Sign up'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+          <View
+            className='justify-center gap-2 flex-row pt-5'>
+            <Text
+              className='text-lg text-gray-100 font-pregular'>
+              Have an account already?
+            </Text>
+            <Link
+              href='/sign-in'
+              className=' text-lg font-psemibold text-secondary'
+            >Sign up</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
