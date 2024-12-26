6777aaf86fdeca0b2a00bcec154289a41e7e4fd0
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-26 22:19:15 +0200

    feat: an environment with methods for processing logic with the database has been created

---
+++ config/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    storageId: process.env.EXPO_PUBLIC_APPWRITE_STORAGE_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
+    videoCollectionId: process.env.EXPO_PUBLIC_APPWRITE_VIDEO_COLLECTION_ID,
+}
