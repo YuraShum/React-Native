ee579a9771189ab2c4c5848b60ddde5dcb9f125c
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-30 18:07:27 +0200

    feat: created search page with functionality

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,13 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList} from 'react-native'
+import React, { useEffect} from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { searchPosts } from '@/lib/appwrite'
+import useAppwrite from '@/hooks/useAppwrite'
+import VideoCard from '@/components/(tabs)/VideoCard'
+import SearchInput from '@/components/(tabs)/SearchInput'
+import EmptyState from '@/components/(tabs)/EmptyState'
+
 
 type Props = {}
 
 const Search = (props: Props) => {
+
+    const { query } = useLocalSearchParams()
+    const { data: posts, refetch } = useAppwrite(() => searchPosts(query))
+
+
+    useEffect(() => {
+        refetch()
+    }, [query])
+
+
     return (
-        <View>
-            <Text>Search</Text>
-        </View>
+        <SafeAreaView
+            className='bg-primary h-full'>
+            <FlatList
+                data={posts}
+                keyExtractor={(item): string => `${item.$id}`}
+                renderItem={({ item }) => (
+                    <VideoCard item={item} />
+                )}
+                ListHeaderComponent={() => (
+                    <View
+                        className='my-6 px-4'>
+                        <Text
+                            className='font-pmedium text-sm text-gray-100'>
+                            Search Results
+                        </Text>
+                        <Text
+                            className='text-2xl font-psemibold text-white'>
+                            {query}
+                        </Text>
+
+                        <View className='mt-6 mb-8'>
+                            {/** search component section */}
+                            <SearchInput initialQuery={query as string} />
+                        </View>
+
+                    </View>
+                )}
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title='No Videos Found'
+                        subtitle='No videos found for this search query' />
+                )} />
+        </SafeAreaView>
     )
 }
 
--- components/(tabs)/SearchInput.tsx
+++ components/(tabs)/SearchInput.tsx
@@ -1,44 +1,59 @@
-import { View, Text, TextInput, TextInputProps, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 
 import { icons } from '@/constants'
+import { router, usePathname } from 'expo-router'
 
 type Props = {
-    title: string,
-    value: string,
-    handleChangeField: (value: string) => void,
-    otherStyles?: string,
-    keyboardType?: TextInputProps['keyboardType'],
+    initialQuery?: string
 }
 
-const SearchInput = ({ title, value, handleChangeField, otherStyles, keyboardType, placeholder }: Props) => {
+const SearchInput = ({initialQuery}: Props) => {
 
-    const [showPassword, setShowPassword] = useState(false)
+    const pathname = usePathname()
+    const [query, setQuery] = useState(initialQuery || '')
     const [isFocused, setIsFocused] = useState(false);
 
     const borderColor = isFocused ? 'border-secondary' : 'border-black-200';
 
+    const handleChangeQuery = (event: string) => {
+        setQuery(event)
+    }
+
+    const onPress = () => {
+        if (!query) {
+            return Alert.alert(
+                'Missing query',
+                "Please input something to search results across database")
+        }
+
+        if (pathname.startsWith('/search')) {
+            router.setParams({ query })
+        }else{
+            router.push(`/search/${query}`)
+        }
+    }
+
     return (
-            <View
-                className={`w-full h-16 px-4 bg-black-100 border-2 ${borderColor} rounded-2xl items-center flex-row space-x-4`}>
-                <TextInput
-                    className='text-base text-white mt-0.5 flex-1 font-pregular'
-                    value={value}
-                    placeholder= 'Search for a video topic'
-                    placeholderTextColor='#7b7b8b'
-                    onChangeText={handleChangeField}
-                    secureTextEntry={title === 'Password' && !showPassword}
-                    keyboardType={keyboardType}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                />
-                <TouchableOpacity>
-                    <Image
+        <View
+            className={`w-full h-16 px-4 bg-black-100 border-2 ${borderColor} rounded-2xl items-center flex-row space-x-4`}>
+            <TextInput
+                className='text-base text-white mt-0.5 flex-1 font-pregular'
+                value={query}
+                placeholder='Search for a video topic'
+                placeholderTextColor='#CDCDE0'
+                onChangeText={(event) => handleChangeQuery(event)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+            />
+            <TouchableOpacity
+                onPress={onPress}>
+                <Image
                     source={icons.search}
                     className='w-5 h-5'
-                    resizeMode='contain'/>
-                </TouchableOpacity>
-            </View>
+                    resizeMode='contain' />
+            </TouchableOpacity>
+        </View>
     )
 }
 
